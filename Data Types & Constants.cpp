e337b453ae3bf0cb744013375d60e43897c30922
Suresh <100891096+Suresh-Dub@users.noreply.github.com>
2023-02-25 10:35:06 +0530

    Create Data Types & Constants.cpp
    C++ Data Types
    Data types define the type of data a variable can hold; for example, an integer variable can hold integer data, a character can hold character data, etc.
    Data types in C++ are categorized into three groups:
    
    Built-in data types 
    These data types are pre-defined for a language and could be used directly by the programmer.
    Examples are: Int, Float, Char, Double, Boolean
    
    User-defined data types
    These data types are defined by the user itself.
    Examples are: Class, Struct, Union, Enum
    
    Derived data types
    These data types are derived from the primitive built-in data types.
    Examples are: Array, Pointer, Function

---
+++ Data Types & Constants.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 17, b = 130;
+    float pi = 3.1756;
+    char c = 'd';
+    cout<<"The Number is "<<a<<".\n The value of b is "<< b;
+    cout<<"\n The valur of pi is "<<pi;
+    cout<<"\n The Value of c is "<<c;
+    return 0;
+}
