f10318d9c7b5029b84c770642dad42eb722385b3
Suresh <100891096+Suresh-Dub@users.noreply.github.com>
2023-02-25 12:18:13 +0530

    Create Input Output.cpp
    Input stream - 
    In the input stream, the direction of the flow of bytes occurs from the input device (for ex keyboard) to the main memory.
    
    Output stream - 
    In the output stream, the direction of flow of bytes occurs from the main memory to the output device (for ex-display).

---
+++ Input Output.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<"Enter the Number A = ";
+    cin>>a;
+    cout<<"Enter the Number B = ";
+    cin>>b;
+    cout<<"The Number A is = "<<a<<endl;
+    cout<<"The Number B is = "<<b<<endl;
+
+    return 0;
+}
